096a22e589bb04b4fd008d8f115f7012237def2e
lompo <lompo@iuj.ac.jp>
2020-10-16 06:51:09 +0900

    imageuploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_200536) do
+ActiveRecord::Schema.define(version: 2020_10_15_213937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_09_27_200536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "icon"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
