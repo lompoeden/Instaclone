7b7c650ee36af17d01713691e000bc365b40ec02
lompo <lompo@iuj.ac.jp>
2020-10-23 15:37:04 +0900

    favorites url fixed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    favorite = current_user.favorites.destroy.(post_id: params[:post_id])
     redirect_to posts_url, notice: "#you have deleted like for {favorite.post.user.name}'s post"
   end
 end
